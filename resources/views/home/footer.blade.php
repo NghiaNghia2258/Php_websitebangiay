409ec0edc5b087798612a63987130d3f93d3532e
NghiaNghia <ngonghia2258@gmail.com>
2024-05-25 18:01:58 +0700

    first commit

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -10,7 +10,7 @@
                             </div>
                             <div class="copywrite_text d-flex align-items-center">
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">Đỗ Như Nghĩa</a></a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">Phan văn lộc</a></a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
                         </div>
